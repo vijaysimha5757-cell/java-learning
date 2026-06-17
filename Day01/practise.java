81cc9dfca3bbe0556dbb7c67b15fc0be961cc552
vijaysimha5757-cell <vijaysimha5757@gmail.com>
2026-06-17 20:04:07 +0530

    Add files via upload
    program using variables

---
+++ Day01/practise.java
@@ -0,0 +1,12 @@
+public class practise{
+    public static void main (String[]args){
+        String Name = "Vijay"; 
+        String Branch = "cse";
+        int Semister = 7;
+        double CGPA = 8.2;
+        System.out.println(Name);
+        System.out.println(Branch);
+        System.out.println(Semister);
+        System.out.println("CGPA:"+CGPA);
+    }
+}
