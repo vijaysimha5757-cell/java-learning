c55e830508954dd9239c8ae2bce19f402b6266a8
vijaysimha5757-cell <vijaysimha5757@gmail.com>
2026-06-17 20:02:24 +0530

    Add files via upload
    first java program

---
+++ Day01/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main (String[]args){
+        String name = "vijay";
+        int age = 21;
+        double cgpa = 8.2;
+        boolean placed = false;
+
+        System.out.println("age:"+age);
+        System.out.println("name"+name);
+        System.out.println("cgpa"+cgpa);
+        System.out.println(placed);
+    }
+}
