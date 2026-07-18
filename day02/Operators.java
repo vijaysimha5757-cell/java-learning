f61e701e567797a1adbdc921ee2c41948498f638
vijaysimha5757-cell <vijaysimha5757@gmail.com>
2026-07-18 12:50:54 +0530

    Add files via upload

---
+++ day02/Operators.java
@@ -0,0 +1,11 @@
+public class Operators{
+    public static void main(String[] args) {
+        int a =20;
+        int b =6;
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Subtraction:"+(a-b));
+        System.out.println("Multiplication:"+(a*b));
+        System.out.println("Division:"+(a/b));
+        System.out.println("Remainder:"+(a%b));
+    }
+}
